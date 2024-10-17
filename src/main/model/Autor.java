83e0a0933069d0fd224ebf46715ce12280cb3b7f
LucaRosendo <lucas115rosendo@gmail.com>
2024-10-17 09:17:51 -0300

    Intrudução de algumas classes

---
+++ src/main/model/Autor.java
@@ -0,0 +1,52 @@
+
+import java.util.List;
+
+public class Autor {
+
+    private String nome;
+    private List<String> publicacoes;
+    private String descricao;
+    private String avaliacaoAutor;
+
+    public Autor(String nome, List<String> publicacoes, String descricao, String avaliacaoAutor) {
+        this.nome = nome;
+        this.publicacoes = publicacoes;
+        this.descricao = descricao;
+        this.avaliacaoAutor = avaliacaoAutor;
+    }
+
+    public void adicionarLivro(String livro) {
+        this.publicacoes.add(livro);
+    }
+
+    public void definirAvaliacao(String avaliacao) {
+        this.avaliacaoAutor = avaliacao;
+    }
+
+    public List<String> listarPublicacoes() {
+        return this.publicacoes;
+    }
+
+    public String obterAvaliacao() {
+        return this.avaliacaoAutor;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public static void main(String[] args) {
+        Autor autor = new Autor("J.K. Rowling",
+                List.of("Harry Potter e a Pedra Filosofal",
+                        "Harry Potter e a Câmara Secreta"),
+                "Escritora britânica", "5 estrelas");
+
+        System.out.println(autor.getNome());
+        System.out.println(autor.listarPublicacoes());
+        System.out.println(autor.obterAvaliacao());
+    }
+}
